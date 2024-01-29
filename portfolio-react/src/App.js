7f617e0866205691f710a54436c98f5accea589c
Jacob Yaws <Jacob.Yaws@du.edu>
2024-01-28 21:23:20 -0700

    Updated styling to meet end requirements at this time.

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -10,55 +10,37 @@ import React, { useState, useEffect} from 'react';
 
 
 function App() {
-  // const [projectsArray, setProjectsArray] = useState([]);
-  //   // const imageMap = new Map()
-  //   // imageMap.set(616700957, image616700957)
-  //   // imageMap.set(664301406, image)
+  const [projectsArray, setProjectsArray] = useState([]);
+    // const imageMap = new Map()
+    // imageMap.set(616700957, image616700957)
+    // imageMap.set(664301406, image)
 
 
-  //   useEffect(() => {
-  //       gitFetch()
-  //   }, [])
+    useEffect(() => {
+        gitFetch()
+    }, [])
 
-  //   const excludes = [566133333, 552179195]; //Excluding the portfolio project (this project) and a forked repository without any code that cannot be set to private
-  //   const gitFetch = async () => {
-  //       const projObjectArray = [];
+    const excludes = [566133333, 552179195]; //Excluding the portfolio project (this project) and a forked repository without any code that cannot be set to private
+    const gitFetch = async () => {
+        const projObjectArray = [];
 
-  //       const response = await (fetch(`https://api.github.com/users/JacobYaws/repos`)
-  //       .then(response => response.json())
-  //       .then(data => {
+        const response = await (fetch(`https://api.github.com/users/JacobYaws/repos`)
+        .then(response => response.json())
+        .then(data => {
            
-  //           console.log(data)
-  //           const projects = data?.forEach((project) => {
-  //           let projectId = project.id
-  //           console.log(projectId)
-  //           const projObject = { name: project.name, id: project.id, url: project.html_url, description: project.description}
-  //           projObjectArray.push(projObject);
-  //           })
-  //           setProjectsArray(projObjectArray)
+            console.log(data)
+            const projects = data?.forEach((project) => {
+            let projectId = project.id
+            console.log(projectId)
+            const projObject = { name: project.name, id: project.id, url: project.html_url, description: project.description}
+            projObjectArray.push(projObject);
+            })
+            setProjectsArray(projObjectArray)
 
 
-  //       }))
-  //   }
+        }))
+    }
   return (
-    // <div className="App">
-    //   {/* <Navbar>
-    //     <Nav>
-    //       <Nav.Link id="main-link">Home Button</Nav.Link>
-    //       <Nav.Link className="secondary-link">About Me</Nav.Link>
-    //       <Nav.Link className="secondary-link">Work</Nav.Link>
-    //       <Nav.Link className="secondary-link">Contact</Nav.Link>
-    //     </Nav>
-    //   </Navbar> */}
-    //   <Container className="main-container">
-    //     <AboutMeAVI />
-    //     <ProjectResults />
-    //     <SkillsEdu />
-    //     <ContactInfo />
-      
-    //   </Container>
-      
-    // </div>
 
     <Container className='body'>
       <header className='header'>
@@ -74,14 +56,18 @@ function App() {
       </header>
 
       <Container className='home' id="aboutme">
-          <Col md='6' className='home-info'>
+          <Col md='5' className='home-info'>
           <h1>Jacob Yaws</h1>
           <h3>Full Stack Web Developer</h3>
-          <p>I am looking to start a career in web development. 
-                I formerly worked as a team lead in transportation and a test technician for an autonomous yard truck company. 
-                I am eager to start applying my skills and learning new ones in the tech industry.</p>
+                <p>Welcome to my page! 
+                My name is Jacob Yaws and I am looking to start a career in web development. 
+                I am a former transportation team lead and former test technician in autonomy looking to start a career focused on software development.
+                I have enjoyed the challenge of learning how to code and I am excited to keep learning new ways to improve and learn new ways to enhance my skills.
+                I am eager to apply my skillset and help build new and exciting applications.
+                 </p>
+  
           </Col>
-          <Col className="home-img-col"md='5'>
+          <Col className="home-img-col" md='5'>
             <Image className="home-img" src={require('./assets/profile/IMG_2545.jpg')}/>
           </Col>
       </Container>
@@ -91,7 +77,7 @@ function App() {
         <Container className="project-results">
           <h3>Personal Work</h3>
           <Carousel>
-              {/* {projectsArray.map((element) => (
+              {projectsArray.map((element) => (
                   // <div>{element.name}</div>
                   
               <Carousel.Item key={element.id} >
@@ -104,18 +90,8 @@ function App() {
                       </Carousel.Caption>
               </Carousel.Item>
               )
-          )} */}
+          )}
 
-                                    <Carousel.Item >
-                                    <Link>
-                                    <Image className="carousel-img" src={require(`./assets/test/stock.png`)} />
-                                    
-                                    </Link>
-                                        <Carousel.Caption>
-                                        <h2 className="caption-text">name</h2>
-                                        <p className="caption-text">description</p>
-                                        </Carousel.Caption>
-                                    </Carousel.Item>
           </Carousel>
           </Container>
       </Container>
@@ -130,14 +106,17 @@ function App() {
               <li>MySQL</li>
               <li>HTML5</li>
               <li>CSS</li>
-              <li>Linux Systems</li>
+              <li>Linux</li>
+              <li>Robotics</li>
+              <li>Troubleshooting</li>
+              <li>Problem Solving</li>
               </ul>
             </Col>
             <Col className="edu-col">
               <ul className="edu">
               {/* <li>Kansas State University <span>Bachelor of Science - Agricultural Technology and Management</span></li> */}
-              <li>Kansas State University - Agricultural Technology and Management</li>
-              <li>Full Stack Coding Bootcamp - University of Denver</li>
+              <li className="li-span">Agricultural Technology & Management - <span>Kansas State University</span></li>
+              <li className="li-span">Full Stack Coding Bootcamp - <span>University of Denver</span></li>
               </ul>
             </Col>
           </Row>
@@ -150,9 +129,6 @@ function App() {
             <li><a href="https://github.com/JacobYaws">Github</a></li>
             <li><a href="www.linkedin.com/in/jacob-yaws">LinkedIn</a></li>
             <li><a href="mailto:jacobyaws@gmail.com">jacobyaws@gmail.com</a></li>
-            {/* <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
-  Popover on top
-</button> */}
           </ul>
         </Container>
       </Container>
